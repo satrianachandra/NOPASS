a60a7f48efa1f3e8f3601e2dbfb68a369e5b7338
Chandra Satriana <satrianachandra@gmail.com>
2014-10-10 14:54:51 +0200

    Add Thread Pool

--- src/server/MultiThreadedTCPServer.java
+++ src/server/MultiThreadedTCPServer.java
@@ -8,6 +8,8 @@ package server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import logger.MyLogger;
@@ -22,11 +24,15 @@ public class MultiThreadedTCPServer implements Runnable{
     protected ServerSocket serverSocket = null;
     protected boolean      isStopped    = false;
     protected Thread       runningThread= null;
+    protected ExecutorService threadPool ;// Executors.newFixedThreadPool(30);
+    private int sizeOfThreadPool = 10;
     private static final Logger LOGGER =
         Logger.getLogger(MultiThreadedTCPServer.class.getName());
     
-    public MultiThreadedTCPServer(int port){
+    public MultiThreadedTCPServer(int port, int sizeOfThreadPool){
         this.serverPort = port;
+        this.sizeOfThreadPool = sizeOfThreadPool;
+        this.threadPool= Executors.newFixedThreadPool(sizeOfThreadPool);
     }
 
     public void run(){
@@ -50,11 +56,10 @@ public class MultiThreadedTCPServer implements Runnable{
                 throw new RuntimeException(
                     "Error accepting client connection", e);
             }
-            new Thread(
-                new ClientThread(
-                    clientSocket, "Multithreaded Server")
-            ).start();
+            this.threadPool.execute(new ClientThread(
+                    clientSocket, "Multithreaded Server"));
         }
+        this.threadPool.shutdown();
         //System.out.println("Server Stopped.") ;
         LOGGER.log(Level.INFO, "Server Stopped");
         LOGGER.exiting(getClass().getName(), "Server thread stopped");
@@ -93,15 +98,18 @@ public class MultiThreadedTCPServer implements Runnable{
             } catch (IOException ex) {
                 LOGGER.log(Level.SEVERE, "Cannot Open file", ex);
         }
+        
+        int sizeOfThreadPool=10;
         if (args.length == 0){
             throw new IllegalArgumentException("Must specify a port!");
-	}else if (args.length==2){
+	}else if (args.length>=2){
             String level = args[1];
             MyLogger.setLevel(Integer.decode(level));
+            sizeOfThreadPool = Integer.decode(args[2]);
         }
         
 	int port = Integer.parseInt(args[0]);
-        MultiThreadedTCPServer server = new MultiThreadedTCPServer(port);
+        MultiThreadedTCPServer server = new MultiThreadedTCPServer(port,sizeOfThreadPool);
         new Thread(server).start();
     
                 
