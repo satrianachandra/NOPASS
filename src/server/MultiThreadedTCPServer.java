c36f0c4eb29fa0decd04a5f0c4372dc0732158cb
Chandra Satriana <satrianachandra@gmail.com>
2014-10-26 19:02:30 +0100

    ä

--- src/server/MultiThreadedTCPServer.java
+++ src/server/MultiThreadedTCPServer.java
@@ -131,7 +131,7 @@ public class MultiThreadedTCPServer implements Runnable{
         }
         
 	int port = Integer.parseInt(args[0]);
-        MultiThreadedTCPServer server = new MultiThreadedTCPServer(port,1000);
+        MultiThreadedTCPServer server = new MultiThreadedTCPServer(8181,1000);
         new Thread(server).start();
         
     
