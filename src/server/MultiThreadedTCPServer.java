605961e41412aafd64b0c36e601710b23989d09a
Chandra Satriana <satrianachandra@gmail.com>
2014-10-26 14:53:20 +0100

    threadool 50

--- src/server/MultiThreadedTCPServer.java
+++ src/server/MultiThreadedTCPServer.java
@@ -131,7 +131,7 @@ public class MultiThreadedTCPServer implements Runnable{
         }
         
 	int port = Integer.parseInt(args[0]);
-        MultiThreadedTCPServer server = new MultiThreadedTCPServer(port,250);
+        MultiThreadedTCPServer server = new MultiThreadedTCPServer(port,50);
         new Thread(server).start();
         
     
